7ee62ed52ab34aff45f864071849377e068fcfe7
zakariae2005 <lzakariae1937@gmail.com>
2025-06-09 17:23:27 +0200

    prisma client

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,6 +3,7 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
+
 const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
